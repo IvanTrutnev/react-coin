b839606f0f7612f98e082c379bfba67faf06c513
Ivan Trutnev <i@gmail.com>
2018-04-19 16:54:26 +0300

    add information to detail component

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-
+import React from 'react';
 /**
   * Fetch error helper
   *
@@ -9,4 +9,20 @@ export const handlerResponse = (response) => {
   return response.json().then(json => {
     return response.ok ? json : Promise.reject(json);
   })
+}
+
+/**
+  * Render change percent helper
+  *
+  * @param {string} percent
+**/
+
+export const renderChangePercent = (percent) => {
+    if (percent > 0) {
+      return <span className="percent-raised">{percent}% &uarr;</span>
+    } else if (percent < 0) {
+      return <span className="percent-fallen">{percent}% &darr;</span>
+    } else {
+      return <span>{percent}</span>
+    }
 }
